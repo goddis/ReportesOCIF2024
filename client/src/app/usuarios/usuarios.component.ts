59728f06f4a8fb795a5ce0dd3a871a182ad1b19b
Manuela Hernandez Rios <manherna@bancolombia.com.co>
2024-01-19 11:45:47 -0500

    agrego funcionalidad para exportar tabla en excel y pdf: tabla usuarios

--- client/src/app/usuarios/usuarios.component.ts
+++ client/src/app/usuarios/usuarios.component.ts
@@ -1,19 +1,28 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import * as XLSX from 'xlsx';
+import * as jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
-import { MatPaginator, MatPaginatorModule,  MatPaginatorIntl  } from '@angular/material/paginator';
+import {
+  MatPaginator,
+  MatPaginatorModule,
+  MatPaginatorIntl,
+} from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
-import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 import { UsuariosService } from '../services/usuarios-service.service';
 import { Usuario } from '../interfaces/Usuario';
 import { colors } from '../../styles';
-import { HttpClientModule } from '@angular/common/http';
+
 import { FormDialogComponent } from '../form-dialog/form-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-usuarios',
@@ -26,33 +35,49 @@ import { MatDialog } from '@angular/material/dialog';
     MatIconModule,
     MatPaginatorModule,
     MatSortModule,
+    MatGridListModule,
     HttpClientModule,
-    FormDialogComponent
+    FormDialogComponent,
   ],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css',
 })
 export class UsuariosComponent implements AfterViewInit {
+ 
   displayedColumns: string[] = [
     'username',
     'number_id',
     'name',
     'lastname',
-    'area_id',
-    'tipo_grupo_seguridad_id',
-    'acciones'
+    'area_name',
+    'grupo_seguridad',
+    'acciones',
   ];
   dataSource = new MatTableDataSource<Usuario>();
+  columnasExp: string[] = [];
   black = colors.black;
   white = colors.white;
+  blue = colors.blue;
 
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
   @ViewChild(MatSort)
   sort!: MatSort;
 
-  constructor(private usuariosService: UsuariosService, public dialog: MatDialog) {
+  constructor(
+    private usuariosService: UsuariosService,
+    public dialog: MatDialog
+  ) {
     this.dataSource = new MatTableDataSource();
+    // columnas que se van a exportar en los archivos
+    this.columnasExp = [
+      'Usuario',
+      'Cédula',
+      'Nombre',
+      'Apellido',
+      'Área',
+      'Grupo de seguridad',
+    ];
   }
 
   ngOnInit(): void {
@@ -82,12 +107,64 @@ export class UsuariosComponent implements AfterViewInit {
     });
   }
 
+  // funcion para exportar la tabla en excel
+  exportarExcel() {
+    // nombre del archivo que se descarga
+    const excelName = 'Usuarios.xlsx';
+    // se obtienen los datos de la tabla (se filtra del datasource solo las columnas que se exportan)
+    const datosTabla = this.dataSource.data.map((row) => [
+      row.username,
+      row.number_id,
+      row.name,
+      row.lastname,
+      row.area_name,
+      row.grupo_seguridad,
+    ]);
+    // se obtienen los datos con sus columnas
+    datosTabla.splice(0, 0, this.columnasExp);
+
+    // se genera el workbook y la worksheet
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(datosTabla);
+    XLSX.utils.book_append_sheet(wb, ws, 'Usuarios');
+
+    // se guarda el archivo excel
+    XLSX.writeFile(wb, excelName);
+  }
+
+  // funcion para exportar la tabla en pdf
+  exportarPDF() {
+    const pdfName = 'Usuarios.pdf';
+    const doc = new jsPDF.default();
+    // se obtienen los datos de la tabla (se filtra del datasource solo las columnas que se exportan)
+    const datosTabla = this.dataSource.data.map((row) => [
+      row.username,
+      row.number_id,
+      row.name,
+      row.lastname,
+      row.area_name,
+      row.grupo_seguridad,
+    ]);
+    // se genera la tabla en el documento pdf
+    autoTable(doc, {
+      head: [this.columnasExp],
+      body: datosTabla,
+      styles: {
+        cellPadding: 1, // espacio entre el contenido de la celda y el borde
+        fontSize: 10,
+        valign: 'middle', // alineacion vertical
+        halign: 'center' // alineacion horizontal
+      }
+    });
+    // se descarga el archivo pdf
+    doc.save(pdfName);
+  }
+
   onEdit() {
     this.openDialog();
     console.log('click en editar');
   }
 
-  
   onDelete() {
     // this.openDialog();
     console.log('click en eliminar');
@@ -106,3 +183,4 @@ export class UsuariosComponent implements AfterViewInit {
 export class CustomMatPaginatorIntl extends MatPaginatorIntl {
   override itemsPerPageLabel = 'Registros por página';
 }
+
